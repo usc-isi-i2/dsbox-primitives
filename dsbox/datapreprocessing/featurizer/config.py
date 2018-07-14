0fd4efa4824f6b8cc8d4d0d0ae3746c5329954b8
ckxz105 <liu494@usc.edu>
2018-07-13 20:31:44 -0700

    change version to d3m v2018.7.10

--- dsbox/datapreprocessing/featurizer/config.py
+++ dsbox/datapreprocessing/featurizer/config.py
@@ -1,8 +1,8 @@
 import os
 from d3m import utils
 
-D3M_API_VERSION = '2018.6.5'
-VERSION = "0.1.6"
+D3M_API_VERSION = '2018.7.10'
+VERSION = "0.2.0"
 TAG_NAME = "{git_commit}".format(git_commit=utils.current_git_commit(os.path.dirname(__file__)), )
 
 REPOSITORY = "https://github.com/usc-isi-i2/dsbox-featurizer"
