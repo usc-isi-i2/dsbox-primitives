16ea91b5c792e410ddf8fd9fb2b192b7cf52406d
liangmuxin <1441793911@qq.com>
2019-01-17 15:59:27 -0800

    change hashtag

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(name='dsbox-featurizer',
           'dsbox-datacleaning==1.3.0', "pyramid-arima==0.8.1"
       ],
       dependency_links=[
-        'git+https://github.com/usc-isi-i2/dsbox-cleaning.git@470a499413c314e658a2c0f136297bd6e919f26d#egg=dsbox-datacleaning-1.3.0'
+        'git+https://github.com/usc-isi-i2/dsbox-cleaning.git@3ea707641ac96825746814dda7c63d4d271c8d1f#egg=dsbox-datacleaning-1.3.0'
       ],
       keywords='d3m_primitive',
       entry_points = {
