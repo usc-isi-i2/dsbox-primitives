55b0478a4935d9ee413baf84fa8085cc05d8370e
ckxz105 <liu494@usc.edu>
2019-03-12 17:20:49 -0700

    update unit-test and generate_primitive_json logic

--- dsbox/generate-primitive-json.py
+++ dsbox/generate-primitive-json.py
@@ -6,33 +6,41 @@ import subprocess
 from dsbox.datapreprocessing.featurizer import config as featurizer_config
 
 parser = argparse.ArgumentParser(description='Generate primitive.json descriptions')
-parser.add_argument(
-    'dirname', action='store', help='Top-level directory to store the json descriptions, i.e. primitives_repo directory')
+parser.add_argument('dirname', action='store', help='Top-level directory to store the json descriptions, i.e. primitives_repo directory', default = 'output')
 arguments = parser.parse_args()
 
 PREFIX = 'd3m.primitives.'
-PRIMITIVES = [
-    ('feature_extraction.resnet50_image_feature.DSBOX', featurizer_config),
-    ('feature_extraction.vgg16_image_feature.DSBOX', featurizer_config),
-    ('feature_extraction.multitable_featurization.DSBOX', featurizer_config),
-    ('data_preprocessing.dataframe_to_tensor.DSBOX', featurizer_config),
-    ('feature_extraction.random_projection_timeseries_featurization.DSBOX', featurizer_config),
-    ('data_preprocessing.time_series_to_list.DSBOX', featurizer_config),
-    ('data_preprocessing.do_nothing.DSBOX', featurizer_config),
-    ('data_preprocessing.do_nothing_for_dataset.DSBOX', featurizer_config),
-    ('time_series_forecasting.rnn_time_series.DSBOX', featurizer_config),
-    ('time_series_forecasting.arima.DSBOX', featurizer_config),
-    ('data_transformation.group_up_by_timeseries.DSBOX', featurizer_config)
-]
+PRIMITIVES = []
 
+try:
+    f = open("../setup.py","r")
+except:
+    f = open("setup.py","r")
 
+line = f.readline()
+while line:
+    line = f.readline()
+    if "'d3m.primitives': [" in line:
+        line = f.readline()
+        while line and "]," not in line:
+            try:
+                primitive_name = line.split("=")[0]
+                primitive_name = primitive_name.split("'")[1]
+                if primitive_name[-1] == " ":
+                    primitive_name = primitive_name[:-1]
+                PRIMITIVES.append(primitive_name)
+                line = f.readline()
+            except:
+                break
+        break
+f.close()
 
-for p, config in PRIMITIVES:
+for p in PRIMITIVES:
     print('Generating json for primitive ' + p)
     primitive_name = PREFIX + p
-    outdir = os.path.join(arguments.dirname, 'v'+config.D3M_API_VERSION,
-                       config.D3M_PERFORMER_TEAM, primitive_name,
-                       config.VERSION)
+    outdir = os.path.join(arguments.dirname, 'v'+featurizer_config.D3M_API_VERSION,
+                       featurizer_config.D3M_PERFORMER_TEAM, primitive_name,
+                       featurizer_config.VERSION)
     subprocess.run(['mkdir', '-p', outdir])
 
     json_filename =  os.path.join(outdir, 'primitive.json')
