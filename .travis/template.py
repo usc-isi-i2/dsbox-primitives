9169dd7e1113f6665f50220a4c51b903245ad441
liangmuxin <1441793911@qq.com>
2019-03-20 12:59:20 -0700

    name

--- .travis/template.py
+++ .travis/template.py
@@ -408,7 +408,7 @@ class simple_config:
 
 DATASET_MAPPER = {
             'classification': "38_sick",
-            'regression': "196_auto_mpg", 
+            'regression': "196_autoMpg", 
             'timeseries': "uu1_datasmash"
         }
 def _product_dict(dct):
