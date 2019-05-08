3b8219c93e195aef6a7c2e69fe601335ddcd4823
ckxz105 <liu494@usc.edu>
2019-05-08 14:26:54 -0700

    remove tensorflow-gpu

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@master#egg=d3m --progress-bar off
-pip uninstall -y tensorflow-gpu 
 pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives@master#egg=common-primitives --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
+pip uninstall -y tensorflow-gpu 
 pip install -e . --progress-bar off
 pip install -e git+https://github.com/brekelma/dsbox_corex@master#egg=dsbox_corex --progress-bar off
 
