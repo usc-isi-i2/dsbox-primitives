d179c45c6749d37dcd69a7ffc9ef3396d526e68b
ckxz105 <liu494@usc.edu>
2019-05-08 14:50:34 -0700

    install tensorflow cpu version!

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -4,6 +4,7 @@ pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives@mast
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
 pip uninstall -y tensorflow-gpu 
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
+pip install tensorflow==1.13.0
 pip list
 pip install -e . --progress-bar off
 pip install -e git+https://github.com/brekelma/dsbox_corex@master#egg=dsbox_corex --progress-bar off
