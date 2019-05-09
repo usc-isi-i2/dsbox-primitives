1203a871482486d5c6d5906789dd8afb8d3b33d3
ckxz105 <liu494@usc.edu>
2019-05-09 13:46:18 -0700

    update entry_point

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -5,7 +5,6 @@ pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=
 pip uninstall -y tensorflow-gpu 
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
 pip install tensorflow==1.12.0
-pip list
 pip install -e . --progress-bar off
 pip install -e git+https://github.com/brekelma/dsbox_corex@master#egg=dsbox_corex --progress-bar off
-
+pip list
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools.command.install import install
 from setuptools.command.develop import develop
-
+from .entry_points import entry_points
 
 with open('requirements.txt', 'r') as f:
     install_requires = list()
@@ -63,42 +63,7 @@ setup(name='dsbox-primitives',
       install_requires=install_requires,
       keywords='d3m_primitive',
       entry_points={
-          'd3m.primitives': [
-              'data_cleaning.cleaning_featurizer.DSBOX = dsbox.datapreprocessing.cleaner:CleaningFeaturizer',
-              'data_preprocessing.encoder.DSBOX = dsbox.datapreprocessing.cleaner:Encoder',
-              'data_preprocessing.unary_encoder.DSBOX = dsbox.datapreprocessing.cleaner:UnaryEncoder',
-              'data_preprocessing.greedy_imputation.DSBOX = dsbox.datapreprocessing.cleaner:GreedyImputation',
-              'data_preprocessing.iterative_regression_imputation.DSBOX = dsbox.datapreprocessing.cleaner:IterativeRegressionImputation',
-              'data_preprocessing.mean_imputation.DSBOX = dsbox.datapreprocessing.cleaner:MeanImputation',
-              'normalization.iqr_scaler.DSBOX = dsbox.datapreprocessing.cleaner:IQRScaler',
-              'data_cleaning.labeler.DSBOX = dsbox.datapreprocessing.cleaner:Labler',
-              'normalization.denormalize.DSBOX = dsbox.datapreprocessing.cleaner:Denormalize',
-              'schema_discovery.profiler.DSBOX = dsbox.datapreprocessing.cleaner:Profiler',
-              'data_cleaning.column_fold.DSBOX = dsbox.datapreprocessing.cleaner:FoldColumns',
-              'data_preprocessing.vertical_concat.DSBOX = dsbox.datapostprocessing:VerticalConcat',
-              'data_preprocessing.ensemble_voting.DSBOX = dsbox.datapostprocessing:EnsembleVoting',
-              'data_preprocessing.unfold.DSBOX = dsbox.datapostprocessing:Unfold',
-              'data_preprocessing.splitter.DSBOX = dsbox.datapreprocessing.cleaner:Splitter',
-              'data_preprocessing.horizontal_concat.DSBOX = dsbox.datapostprocessing:HorizontalConcat',
-              'data_transformation.to_numeric.DSBOX = dsbox.datapreprocessing.cleaner:ToNumeric',
-              'data_preprocessing.do_nothing.DSBOX = dsbox.datapreprocessing.featurizer.pass:DoNothing',
-              'data_preprocessing.do_nothing_for_dataset.DSBOX = dsbox.datapreprocessing.featurizer.pass:DoNothingForDataset',
-              'feature_extraction.multitable_featurization.DSBOX = dsbox.datapreprocessing.featurizer.multiTable:MultiTableFeaturization',
-              'data_preprocessing.dataframe_to_tensor.DSBOX = dsbox.datapreprocessing.featurizer.image:DataFrameToTensor',
-              'feature_extraction.yolo.DSBOX = dsbox.datapreprocessing.featurizer.image:Yolo',
-              'feature_extraction.lstm.DSBOX = dsbox.datapreprocessing.featurizer.image:LSTM',
-              'feature_extraction.vgg16_image_feature.DSBOX = dsbox.datapreprocessing.featurizer.image:Vgg16ImageFeature',
-              'feature_extraction.inceptionV3_image_feature.DSBOX = dsbox.datapreprocessing.featurizer.image:InceptionV3ImageFeature',
-              'feature_extraction.resnet50_image_feature.DSBOX = dsbox.datapreprocessing.featurizer.image:ResNet50ImageFeature',
-              'data_preprocessing.time_series_to_list.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:TimeseriesToList',
-              'feature_extraction.random_projection_timeseries_featurization.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:RandomProjectionTimeSeriesFeaturization',
-              'data_transformation.group_up_by_timeseries.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:GroupUpByTimeSeries',
-              'time_series_forecasting.arima.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:AutoArima',
-              'time_series_forecasting.rnn_time_series.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:RNNTimeSeries',
-              'data_augmentation.wikifier.DSBOX = dsbox.datapreprocessing.cleaner:Wikifier',
-              'data_augmentation.datamart_download.DSBOX = dsbox.datapreprocessing.cleaner:DatamartDownload',
-              'data_augmentation.datamart_augmentation.DSBOX = dsbox.datapreprocessing.cleaner:DatamartAugmentation',
-          ],
+          'd3m.primitives': entry_points,
       },
       cmdclass={
           'develop': PostDevelopCommand,
