fd03a2e7148dfec6ffdada6d1ec04195c4221f2d
liangmuxin <1441793911@qq.com>
2019-05-16 13:26:26 -0700

    change way of installation

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@master#egg=d3m --progress-bar off
-pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives@master#egg=common-primitives --progress-bar off
+pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@8ddad7b810896de481c25b57d70c63d189ce99f0#egg=common_primitives --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
 pip uninstall -y tensorflow-gpu 
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
