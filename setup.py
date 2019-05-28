4ed846a12e2cccec2d4ad132d7bf653ca6202b3c
Ke-Thia Yao <kyao@isi.edu>
2019-05-28 10:54:39 -0700

    Remove post setup commands

--- setup.py
+++ setup.py
@@ -1,35 +1,5 @@
 from setuptools import setup
-from setuptools.command.install import install
-from setuptools.command.develop import develop
 
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        # This is used so that it will pass d3m metadata submission process. The dsbox-featurizer package depends on
-        import subprocess
-        result = subprocess.check_output(['pip', 'list'])
-        lines = str(result).split('\\n')
-        for line in lines[2:]:
-            part = line.split()
-            if 'dsbox-featurizer' in part[0]:
-                print(line)
-                if '0' == part[1].split('.')[0]:
-                    subprocess.call(['pip', 'uninstall', '-y', 'dsbox-featurizer'])
-        import keras.applications.resnet50 as resnet50
-        import keras.applications.vgg16 as vgg16
-        resnet50.ResNet50(weights='imagenet')
-        vgg16.VGG16(weights='imagenet', include_top=False)
-        install.run(self)
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        import keras.applications.resnet50 as resnet50
-        import keras.applications.vgg16 as vgg16
-        resnet50.ResNet50(weights='imagenet')
-        vgg16.VGG16(weights='imagenet', include_top=False)
-        develop.run(self)
 
 setup(name='dsbox-primitives',
       version='1.0.0',
@@ -93,8 +63,4 @@ setup(name='dsbox-primitives',
               'time_series_forecasting.arima.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:AutoArima',
               'time_series_forecasting.rnn_time_series.DSBOX = dsbox.datapreprocessing.featurizer.timeseries:RNNTimeSeries'
           ],
-      },
-      cmdclass={
-          'develop': PostDevelopCommand,
-          'install': PostInstallCommand
       })
