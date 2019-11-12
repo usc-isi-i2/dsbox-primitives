8bbc352c48403c97bb1279e7cd253998873b7a5c
ckxz105 <liu494@usc.edu>
2019-11-12 13:42:17 -0800

    use d3m v2017.6.7 for unit test

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@master#egg=d3m --progress-bar off
-pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@master#egg=common_primitives --progress-bar off
+pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@a8af7585fdd85e2218ca88b257bb0ec71adabfb3#egg=d3m --progress-bar off
+pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@5c43e65d306a4f36d53db2fb497c9869e2fb7294#egg=common_primitives --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
 pip uninstall -y tensorflow-gpu
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
