1eb166789cff6d8953ee5a82c25934401bb838ac
Ke-Thia Yao <kethia.yao@gmail.com>
2019-11-26 22:32:07 -0800

    Fix horizontal concat

--- dsbox/datapostprocessing/horizontal_concat.py
+++ dsbox/datapostprocessing/horizontal_concat.py
@@ -1,52 +1,48 @@
-import pandas as pd
-import time
+'''
+Horizontal concatenation of multiple dataframes
+'''
 import typing
 
-import common_primitives.utils as common_utils
-from d3m import container, exceptions
+import pandas as pd
+
+from d3m import container
 from d3m.primitive_interfaces.transformer import TransformerPrimitiveBase
 from d3m.metadata import hyperparams
-from d3m.primitive_interfaces.base import CallResult, MultiCallResult
+from d3m.primitive_interfaces.base import CallResult
 from d3m.metadata.base import ALL_ELEMENTS
-from copy import copy
 
 from dsbox.datapreprocessing.cleaner import config
 
 __all__ = ('HorizontalConcat',)
 
 
-Inputs = container.DataFrame
+Inputs = container.List
 Outputs = container.DataFrame
 
-
 class HorizontalConcatHyperparams(hyperparams.Hyperparams):
-    ignore_index = hyperparams.UniformBool(
+    """Hyperaparams for the HorizontalConcatPrimitive"""
+    use_index = hyperparams.UniformBool(
         default=True,
-        semantic_types=[
-            'https://metadata.datadrivendiscovery.org/types/ControlParameter'],
-        description="Controls whether new df should use original index or not"
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description="Use primary index columns in all DataFrames (if they exist) to match rows in proper order. Otherwise, concatination happens on the order of rows in input DataFrames.",
     )
-    to_semantic_types = hyperparams.Set(
-        elements=hyperparams.Hyperparameter[str](""),
-        default=("https://metadata.datadrivendiscovery.org/types/Attribute",
-                 "https://metadata.datadrivendiscovery.org/types/OrdinalData",
-                 "https://metadata.datadrivendiscovery.org/types/CategoricalData"),
-        semantic_types=[
-            "https://metadata.datadrivendiscovery.org/types/ControlParameter"],
-        description="Sementic typer to add for output dataframe"
+    keep_first_index_only = hyperparams.UniformBool(
+        default=True,
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description="When more than input DataFrames have primary index columns, keep the index from the first DataFrame and remove all other indices from the result."
+                    " When \"use_index\" is \"True\", all other index columns are redundant because they are equal to the first ones (assuming equal metadata).",
     )
-    column_name = hyperparams.Hyperparameter[int](
-        semantic_types=[
-            'https://metadata.datadrivendiscovery.org/types/ControlParameter'],
-        default=100,
-        description="the control params for index name"
+    auto_rename_columns = hyperparams.UniformBool(
+        default=True,
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description="Automatically rename column names, if concatenation results in duplicate column names."
     )
 
 
 class HorizontalConcat(TransformerPrimitiveBase[Inputs, Outputs, HorizontalConcatHyperparams]):
     """
         A primitive which concat a list of dataframe to a single dataframe horizontally,
-        and it will also set metatdata for prediction, 
+        and it will also set metatdata for prediction,
         we assume that inputs has same length
     """
 
@@ -72,118 +68,156 @@ class HorizontalConcat(TransformerPrimitiveBase[Inputs, Outputs, HorizontalConca
         super().__init__(hyperparams=hyperparams)
         self.hyperparams = hyperparams
 
-    def produce(self, *, inputs1: Inputs, inputs2: Inputs,
-                timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
-        # need to rename inputs1.columns and inputs2.columns name
-        if self.hyperparams["column_name"] == 0:
-            left = inputs1.rename(columns={inputs1.columns[-1]: "0"})
-        else:
-            left = copy(inputs1)
-        right = inputs2.rename(
-            columns={inputs2.columns[-1]: str(self.hyperparams["column_name"]+1)})
-        new_df = common_utils.horizontal_concat(left, right)
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+        use_index = self.hyperparams['use_index']
+        keep_first_index_only = self.hyperparams['keep_first_index_only']
+        auto_rename_columns = self.hyperparams['auto_rename_columns']
 
-        for i, column in enumerate(new_df.columns):
-            column_metadata = dict(new_df.metadata.query((ALL_ELEMENTS, i)))
-            if 'https://metadata.datadrivendiscovery.org/types/PrimaryKey' not in column_metadata["semantic_types"]:
-                column_metadata["semantic_types"] = self.hyperparams["to_semantic_types"]
-                new_df.metadata = new_df.metadata.update(
-                    (ALL_ELEMENTS, i), column_metadata)
-        return CallResult(new_df)
+        if len(inputs) == 0:
+            raise ValueError("Input list must have at least one element.")
+        if len(inputs) == 1:
+            return CallResult(inputs[0])
 
-# functions to fit in devel branch of d3m (2019-1-17)
+        first = inputs[0]
+        all_dfs = [first]
+        for other in inputs[1:]:
+            first.metadata._check_same_number_of_samples(other.metadata)
 
-    def set_training_data(self, *, inputs1: Inputs, inputs2: Inputs) -> None:
-        pass
+        first_indices = first.metadata.get_index_columns()
+        column_names = set(first.columns)
+        for other in inputs[1:]:
+            if auto_rename_columns:
+                rename = {}
+                for name in  other.columns:
+                    if name in column_names:
+                        new_name = self._new_name(name, column_names)
+                        rename[name] = new_name
+                        column_names.add(new_name)
+                other = self._rename_columns(other, rename)
+            other_indices = other.metadata.get_index_columns()
+            if first_indices and other_indices:
+                if use_index:
+                    old_other_metadata = other.metadata
+                    other = self._sort_other_indices(first, other, first_indices, other_indices)
+                    # TODO: Reorder metadata rows as well.
+                    #       This should be relatively easy because we can just modify
+                    #       "other.metadata._current_metadata.metadata" map.
+                    other.metadata = old_other_metadata
 
-    def fit_multi_produce(self, *, produce_methods: typing.Sequence[str], inputs1: Inputs, inputs2: Inputs, timeout: float = None, iterations: int = None) -> MultiCallResult:
-        """
-        A method calling ``fit`` and after that multiple produce methods at once.
+            # Removing second primary key columns.
+            if keep_first_index_only:
+                other = other.remove_columns(other_indices)
+            all_dfs.append(other)
+        return CallResult(self._concat_dataframes(all_dfs))
 
-        This method allows primitive author to implement an optimized version of both fitting
-        and producing a primitive on same data.
+    @staticmethod
+    def _rename_columns(dataframe: container.DataFrame, mapping: typing.Dict[str, str]) -> container.DataFrame:
+        '''Rename DataFrame column names, and corresponding metadata column names'''
+        dataframe = dataframe.rename(columns=mapping)
+        for c in range(dataframe.metadata.query((ALL_ELEMENTS,))['dimension']['length']):
+            column_metadata = dataframe.metadata.query((ALL_ELEMENTS, c))
+            if column_metadata['name'] in mapping:
+                column_metadata = dict(column_metadata)
+                column_metadata['name'] = mapping[column_metadata['name']]
+                dataframe.metadata = dataframe.metadata.update((ALL_ELEMENTS, c), column_metadata)
+        return dataframe
 
-        If any additional method arguments are added to primitive's ``set_training_data`` method
-        or produce method(s), or removed from them, they have to be added to or removed from this
-        method as well. This method should accept an union of all arguments accepted by primitive's
-        ``set_training_data`` method and produce method(s) and then use them accordingly when
-        computing results.
+    @staticmethod
+    def _new_name(name: str, existing_names: set):
+        prefix = 'hc'
+        new_name = name
+        while new_name in existing_names:
+            parts = new_name.split('_')
+            if parts[-1][:2] == prefix and parts[-1][2:].isdigit():
+                parts[-1] = prefix + str(int(parts[-1][2:]) + 1)
+            else:
+                parts.append(prefix + '1')
+            new_name = '_'.join(parts)
+        return new_name
 
-        The default implementation of this method just calls first ``set_training_data`` method,
-        ``fit`` method, and all produce methods listed in ``produce_methods`` in order and is
-        potentially inefficient.
+    @staticmethod
+    def _concat_dataframes(inputs: Inputs) -> Outputs:
 
-        Parameters
-        ----------
-        produce_methods : Sequence[str]
-            A list of names of produce methods to call.
-        inputs : Inputs
-            The inputs given to ``set_training_data`` and all produce methods.
-        outputs : Outputs
-            The outputs given to ``set_training_data``.
-        timeout : float
-            A maximum time this primitive should take to both fit the primitive and produce outputs
-            for all produce methods listed in ``produce_methods`` argument, in seconds.
-        iterations : int
-            How many of internal iterations should the primitive do for both fitting and producing
-            outputs of all produce methods.
+        # Do not use Pandas index, just concat based on row position
+        outputs = pd.concat([df.reset_index(drop=True) for df in inputs], axis=1)
 
-        Returns
-        -------
-        MultiCallResult
-            A dict of values for each produce method wrapped inside ``MultiCallResult``.
-        """
+        outputs.metadata = inputs[0].metadata
+        for other in inputs[1:]:
+            outputs.metadata = outputs.metadata.append_columns(other.metadata)
+        return outputs
 
-        return self._fit_multi_produce(produce_methods=produce_methods, timeout=timeout, iterations=iterations, inputs=inputs1, outputs=inputs2)
+    @staticmethod
+    def _sort_other_indices(first: container.DataFrame, right: container.DataFrame, indices: typing.Sequence[int],
+                            right_indices: typing.Sequence[int]) -> container.DataFrame:
+        # Based on DataFrame._sort_right_indices()
 
-    def multi_produce(self, *, inputs1: Inputs, inputs2: Inputs, produce_methods: typing.Sequence[str],
-                      timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
-        results = []
-        for method_name in produce_methods:
-            if method_name != 'produce' and not method_name.startswith('produce_'):
-                raise exceptions.InvalidArgumentValueError(
-                    "Invalid produce method name '{method_name}'.".format(method_name=method_name))
+        # We try to handle different cases.
 
-            if not hasattr(self, method_name):
-                raise exceptions.InvalidArgumentValueError(
-                    "Unknown produce method name '{method_name}'.".format(method_name=method_name))
+        # We do not do anything special. We assume both indices are the same.
+        if len(indices) == 1 and len(right_indices) == 1:
+            # TODO: Handle the case when not all index values exist and "reindex" fills values in: we should fill with NA relevant to the column type.
+            return right.set_index(right.iloc[:, right_indices[0]]).reindex(first.iloc[:, indices[0]]).reset_index(drop=True)
 
-            try:
-                expected_arguments = set(self.metadata.query()['primitive_code'].get(
-                    'instance_methods', {})[method_name]['arguments'])
-            except KeyError as error:
-                raise exceptions.InvalidArgumentValueError(
-                    "Unknown produce method name '{method_name}'.".format(method_name=method_name)) from error
+        index_names = [first.metadata.query_column(index).get('name', None) for index in indices]
+        right_index_names = [right.metadata.query_column(right_index).get('name', None) for right_index in right_indices]
 
-            arguments = {'inputs1': inputs1,
-                         'inputs2': inputs2,
-                         }
+        index_series = [first.iloc[:, index] for index in indices]
+        right_index_series = [right.iloc[:, right_index] for right_index in right_indices]
 
-            start = time.perf_counter()
-            results.append(getattr(self, method_name)(
-                timeout=timeout, **arguments))
-            delta = time.perf_counter() - start
+        # Number match, names match, order match, things look good.
+        if index_names == right_index_names:
+            # We know the length is larger than 1 because otherwise the first case would match.
+            assert len(indices) > 1
+            assert len(indices) == len(right_indices)
 
-            # Decrease the amount of time available to other calls. This delegates responsibility
-            # of raising a "TimeoutError" exception to produce methods themselves. It also assumes
-            # that if one passes a negative timeout value to a produce method, it raises a
-            # "TimeoutError" exception correctly.
-            if timeout is not None:
-                timeout -= delta
+            # TODO: Handle the case when not all index values exist and "reindex" fills values in: we should fill with NA relevant to the column type.
+            return right.set_index(right_index_series).reindex(index_series).reset_index(drop=True)
 
-        # We return the maximum number of iterations done by any produce method we called.
-        iterations_done = None
-        for result in results:
-            if result.iterations_done is not None:
-                if iterations_done is None:
-                    iterations_done = result.iterations_done
-                else:
-                    iterations_done = max(
-                        iterations_done, result.iterations_done)
+        sorted_index_names = sorted(index_names)
+        sorted_right_index_names = sorted(right_index_names)
 
-        return MultiCallResult(
-            values={name: result.value for name,
-                    result in zip(produce_methods, results)},
-            has_finished=all(result.has_finished for result in results),
-            iterations_done=iterations_done,
-        )
+        # Number and names match, but not the order.
+        if sorted_index_names == sorted_right_index_names:
+            # We know the length is larger than 1 because otherwise the first case would match.
+            assert len(indices) > 1
+            assert len(indices) == len(right_indices)
+
+            # We sort index series to be in the sorted order based on index names.
+            index_series = [s for _, s in sorted(zip(index_names, index_series), key=lambda pair: pair[0])]
+            right_index_series = [s for _, s in sorted(zip(right_index_names, right_index_series), key=lambda pair: pair[0])]
+
+            # TODO: Handle the case when not all index values exist and "reindex" fills values in: we should fill with NA relevant to the column type.
+            return right.set_index(right_index_series).reindex(index_series).reset_index(drop=True)
+
+        if len(index_series) == len(right_index_series):
+            # We know the length is larger than 1 because otherwise the first case would match.
+            assert len(indices) > 1
+
+            # logger.warning("Primary indices both on left and right not have same names, but they do match in number.")
+
+            # TODO: Handle the case when not all index values exist and "reindex" fills values in: we should fill with NA relevant to the column type.
+            return right.set_index(right_index_series).reindex(index_series).reset_index(drop=True)
+
+        # It might be that there are duplicate columns on either or even both sides,
+        # but that should be resolved by adding a primitive to remove duplicate columns first.
+        raise ValueError("Left and right primary indices do not match in number.")
+
+
+    # def produce(self, *, inputs1: Inputs, inputs2: Inputs,
+    #             timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+    #     # need to rename inputs1.columns and inputs2.columns name
+    #     if self.hyperparams["column_name"] == 0:
+    #         left = inputs1.rename(columns={inputs1.columns[-1]: "0"})
+    #     else:
+    #         left = copy(inputs1)
+    #     right = inputs2.rename(
+    #         columns={inputs2.columns[-1]: str(self.hyperparams["column_name"]+1)})
+    #     new_df = common_utils.horizontal_concat(left, right)
+
+    #     for i, column in enumerate(new_df.columns):
+    #         column_metadata = dict(new_df.metadata.query((ALL_ELEMENTS, i)))
+    #         if 'https://metadata.datadrivendiscovery.org/types/PrimaryKey' not in column_metadata["semantic_types"]:
+    #             column_metadata["semantic_types"] = self.hyperparams["to_semantic_types"]
+    #             new_df.metadata = new_df.metadata.update(
+    #                 (ALL_ELEMENTS, i), column_metadata)
+    #     return CallResult(new_df)
