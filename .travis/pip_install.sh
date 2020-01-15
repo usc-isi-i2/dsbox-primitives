2156a3f05960164ce2d895c189e5c7fd8de56935
ckxz105 <liu494@usc.edu>
2020-01-15 18:13:51 -0500

    remove install of tensorflow 1.x

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -2,9 +2,9 @@
 pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@feaf49da34568bbf37f82fa6ffeb127631020199#egg=d3m --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@d9ee09a8838a222cead2a093d03c623603e175f9#egg=common_primitives --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
-pip uninstall -y tensorflow-gpu
+# pip uninstall -y tensorflow-gpu
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
-pip install tensorflow==1.12.0
+# pip install tensorflow==2.0.0
 pip install -e . --progress-bar off
 pip install -e git+https://github.com/brekelma/dsbox_corex@master#egg=master --progress-bar off
 pip list
