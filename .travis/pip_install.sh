6863ef43dc2feab3c9c06e5e8d058552ee91ed72
ckxz105 <liu494@usc.edu>
2020-01-17 10:58:18 -0500

    install tensorflow during testing

--- .travis/pip_install.sh
+++ .travis/pip_install.sh
@@ -2,9 +2,9 @@
 pip install -e git+https://gitlab.com/datadrivendiscovery/d3m@master#egg=d3m --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/common-primitives.git@master#egg=common_primitives --progress-bar off
 pip install -e git+https://gitlab.com/datadrivendiscovery/sklearn-wrap@dist#egg=sklearn-wrap --progress-bar off
-# pip uninstall -y tensorflow-gpu
+pip uninstall -y tensorflow-gpu
 export LD_LIBRARY_PATH="$HOME/miniconda/envs/ta1-test-env/lib:$LD_LIBRARY_PATH"
-# pip install tensorflow==2.0.0
+pip install tensorflow==2.0.0
 pip install -e . --progress-bar off
 pip install -e git+https://github.com/brekelma/dsbox_corex@master#egg=master --progress-bar off
 pip list
